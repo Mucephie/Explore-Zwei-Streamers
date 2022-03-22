edfd2c1a5b0b64039f1f24f122df33761d236951
Joshua June Parsons <mucephie@outlook.com>
2022-03-22 01:39:27 -0400

    update

--- mar22_profiles.py
+++ mar22_profiles.py
@@ -9,14 +9,13 @@ from astropy import constants as cnst
 r_s   = 8.8  * un.kpc
 v_h   = 220  * un.km / un.s
 m     = 6E11 * un.Msun
-rho_s = 1    * un.Msun / (un.pc)**3
+rho_s = 0.11    * un.Msun / (un.pc)**3
 r     = np.logspace(-3, 1.1, 256) * un.kpc
 
-gp    = pf.McMillanProfile(r_s = r_s, rho_s = rho_s, v_h = v_h)
+nfwp  = pf.McMillanProfile(r_s = r_s, rho_s = rho_s, v_h = v_h)
 
-density = gp.density(r)
 
-plt.semilogy(r, density, label = 'NFW') # semilogx
+plt.semilogy(r, nfwp.density(r), label = 'NFW') # semilogx
 plt.legend()
 plt.xlabel(r'radius ($kpc$)')
 plt.ylabel(r'mass density ($M_{sol}/{\rm pc}^{3}$)')
